4b2d00aa10064e5ab23c197c3e8985e1a730e271
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-08 11:17:45 -0300

    git

--- src/main/java/com/example/demo/model/Superpoder.java
+++ src/main/java/com/example/demo/model/Superpoder.java
@@ -5,11 +5,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
@@ -24,21 +24,21 @@ public class Superpoder {
     private Long id;
 
     @NotNull(message = "O nome não pode ser nulo.")
-    @Size(min = 3, max = 50, message = "O nome deve ter entre 3V1__criar_tabela_superpoder.sql a 50 caracteres.")
+    @Size(min = 3, max = 50, message = "O nome deve ter entre 3 a 50 caracteres.")
     private String nome;
+
+    @NotNull(message = "A descrição não pode ser nula.")
+    @Size(min = 10, max = 200, message = "A descrição deve ter entre 10 e 200 caracteres.")
+    private String descricao;
 
     @Min(value = 1, message = "A nota mínima é 1.")
     @Max(value = 10, message = "A nota máxima é 10.")
-    private String descricao;
-
-    @Size(min = 1, max = 10, message = "A nota do poder deve ser entre 1 a 10.")
     private Integer nota;
 
-    @NotNull(message = "A razão nao pode ser nula")
+    @NotNull(message = "A razão não pode ser nula.")
     @Size(min = 10, max = 200, message = "A razão deve ter entre 10 e 200 caracteres.")
     private String razao;
 
     @Past(message = "A data de criação não pode ser no futuro.")
     private LocalDate dataCriacao;
-
 }
--- src/main/resources/migration/V1__criar_tabela_superpoder.sql
+++ src/main/resources/migration/V1__criar_tabela_superpoder.sql
@@ -1,5 +1,5 @@
 CREATE TABLE superpoder (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
     descricao VARCHAR(200) NOT NULL,
     nota INT,
